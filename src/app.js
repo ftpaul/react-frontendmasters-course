aa13619d5dadd64fd14cf18541b1e20de7f006d9
ftpaul <ftpaul@gmail.com>
2019-09-07 21:07:35 +0100

    search, hooks, dropdown

--- src/app.js
+++ src/app.js
@@ -1,28 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Adopt-me!"),
-  //   React.createElement(Pet, {
-  //     name: "Horus",
-  //     animal: "dog",
-  //     breed: "Rottweiler"
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Luna",
-  //     animal: "dog",
-  //     breed: "Havanese"
-  //   }),
-  //   React.createElement(Pet, { name: "Jobin", animal: "cat", breed: "Mixed" })
-  // ]);
   return (
     <div>
       <h1>Adopt me!</h1>
-      <Pet name="Horus" animal="Dog" breed="Rottweiler"></Pet>
-      <Pet name="Luna" animal="Dog" breed="Havanese"></Pet>
-      <Pet name="Jobin" animal="Cat" breed="Mixed"></Pet>
+      <SearchParams />
     </div>
   );
 };
